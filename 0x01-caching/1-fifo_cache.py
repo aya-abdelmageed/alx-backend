cf35affc8c363a8e9ed378f252f9a73a6fb9cc9d
aya-abdelmageed <aya.abdelmageed.01@gmail.com>
2024-01-31 23:25:02 +0200

    1

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""1. FIFO caching"""
+from collections import OrderedDict
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """
+    class FIFOCache that inherits from
+    BaseCaching and is a caching system
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """
+        assign to the dictionary self.cache_data
+        the item value for the key key.
+        """
+        if key is None or item is None:
+            return
+
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            first = self.cache_data.popitem(last=False)
+            print(f"DISCARD: {first}")
+
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        return the value in self.cache_data linked to key.
+        """
+        return self.cache_data.get(key, None)
