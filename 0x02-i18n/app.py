e1897246a54f95b68b4d45732c6b8fd534c9374c
aya-abdelmageed <aya.abdelmageed.01@gmail.com>
2024-02-22 09:05:16 +0200

    fix tasks

--- 0x02-i18n/app.py
+++ 0x02-i18n/app.py
@@ -3,6 +3,7 @@
 A Basic Flask app with i18n and l10n
 """
 import pytz
+import datetime
 from typing import Union, Dict
 from flask_babel import Babel, format_datetime
 from flask import Flask, render_template, request, g
